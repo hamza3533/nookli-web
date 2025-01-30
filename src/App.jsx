49712efabc22b786d08f7fadfd2347846323c63d
hamza3533 <hamza.shaikh3533@gmail.com>
2025-01-30 23:33:15 +0500

    google auth added --testing 0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import Login from "./pages/Login/index.jsx";
 import Services from "./pages/Services/index.jsx";
 import AboutUs from "./pages/AboutUs/index.jsx";
 import Careers from "./pages/Careers/index.jsx";
 import ContactUs from "./pages/ContactUs/index.jsx";
+import Dashboard from "./pages/Dashboard/index.jsx"; // Import the Dashboard component
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+import useAuth from "./config/useAuth.js";
 
+function ProtectedRoute({ element, user }) {
+  return user ? element : <Navigate to="/signin" />;
+}
 
 function App() {
+  const user = useAuth();
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/signin" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} user={user} />} />
+        <Route path="/services" element={<ProtectedRoute element={<Services />} user={user} />} />
+        <Route path="/about-us" element={<ProtectedRoute element={<AboutUs />} user={user} />} />
+        <Route path="/careers" element={<ProtectedRoute element={<Careers />} user={user} />} />
+        <Route path="/contact-us" element={<ProtectedRoute element={<ContactUs />} user={user} />} />
       </Routes>
       <Footer />
     </Router>
--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,9 @@
-  import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-  const supabase = createClient("https://huvzrzxvjiafkunnmwxf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1dnpyenh2amlhZmt1bm5td3hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwOTU1NzksImV4cCI6MjA1MzY3MTU3OX0.3c_802W7EJPD-QD-lfYTI5khEUbtcq0wuzGTsunl8ic");
-  // const supabase = createClient("https://txvdfphobjatelthlfbw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dmRmcGhvYmphdGVsdGhsZmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4OTI1NjcsImV4cCI6MjA1MjQ2ODU2N30.dnVGawnoyt8CBkQ36nWkFMkwk03JOHWPZio_SoctyUY");
-  
-  export default supabase;
+const supabase = createClient(
+  "https://ljdqgvvbmwikcwguxmmw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqZHFndnZibXdpa2N3Z3V4bW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNjEyODcsImV4cCI6MjA1MzgzNzI4N30.3YQVIqqSdc6TtVGLfOei7IlQss5agcu7g3fq5FIF3jw"
+);
+// const supabase = createClient("https://txvdfphobjatelthlfbw.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dmRmcGhvYmphdGVsdGhsZmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4OTI1NjcsImV4cCI6MjA1MjQ2ODU2N30.dnVGawnoyt8CBkQ36nWkFMkwk03JOHWPZio_SoctyUY");
+
+export default supabase;
